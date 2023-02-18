dbb2b27e514a66ee32cf9b02ba0d797186f38ceb
Renato Alessandro de Moura <renatomoura@gmail.com>
2023-02-18 13:24:47 -0300

    Initial commit.

---
+++ src/main/java/io/ramoura/howtouse/springboot/grpcservice/HowtouseSpringbootGrpcServiceApplication.java
@@ -0,0 +1,13 @@
+package io.ramoura.howtouse.springboot.grpcservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowtouseSpringbootGrpcServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowtouseSpringbootGrpcServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ramoura/howtouse/springboot/grpcservice/HowtouseSpringbootGrpcServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ramoura.howtouse.springboot.grpcservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowtouseSpringbootGrpcServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
